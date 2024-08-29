43bb7a5b984979c634e7671d89ae111dba20251b
Alice <alicesb2006@gmail.com>
2024-08-29 12:02:08 -0300

    Criando a tela de Welcome e seu estilo temporário

---
+++ src/screens/WelcomeScreen.jsx
@@ -0,0 +1,27 @@
+// Importando dependencias
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function WelcomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome Page!</Text>
+      <Button
+        title="Login"
+        onPress={() => navigation.navigate('LoginScreen')}
+      />
+      <Button
+        title="SignUp"
+        onPress={() => navigation.navigate('SignUp')}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
