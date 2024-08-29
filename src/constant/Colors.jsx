39ed2080080ec8bcbf336623b6037ee0d44f282f
Alice <alicesb2006@gmail.com>
2024-08-29 17:52:52 -0300

    Criando a constantes das cores do projeto

---
+++ src/constant/Colors.jsx
@@ -0,0 +1,8 @@
+const Colors = {
+    ft_color: '#FFF',
+    br_color: '#EDEDED',
+    fill_color1: 'rgba(217, 217, 217, 0.05)',
+    fill_color2: 'rgba(217, 7, 45, 0.36)'
+}
+
+export default Colors
