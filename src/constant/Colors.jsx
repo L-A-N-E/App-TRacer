735749403bc02a40b8cac2fb7464d16b417219d2
Alice <alicesb2006@gmail.com>
2024-09-01 11:38:44 -0300

    Criando novas variáveis de cores

--- src/constant/Colors.jsx
+++ src/constant/Colors.jsx
@@ -1,6 +1,8 @@
 const Colors = {
     ft_color: '#FFF',
     br_color: '#EDEDED',
+    blk: '#000',
+    red_color: '#D9072D',
     fill_color1: 'rgba(217, 217, 217, 0.05)',
     fill_color2: 'rgba(217, 7, 45, 0.36)'
 }
