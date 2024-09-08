11b28b8f9e9df5ee1cba2e658dac22320f7b80f3
Alice <alicesb2006@gmail.com>
2024-09-08 01:17:35 -0300

    Adicionando uma nova fonte

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 // Importando dependências
 import * as React from 'react';
-import { ActivityIndicator, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 // Importando nav
@@ -12,6 +11,7 @@ const App = () => {
   // Carregar fontes 
   const [fontsLoaded] = useFonts({
     'Montserrat': require('./assets/fonts/Montserrat-VariableFont_wght.ttf'),
+    'Dragrace': require('./assets/fonts/Dragrace.ttf')
   });
 
   // Exibir indicador de carregamento enquanto a fonte estão sendo carregadas
