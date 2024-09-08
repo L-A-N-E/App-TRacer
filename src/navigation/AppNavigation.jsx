041f1f56533ce7a9cda34c719f064247c6c9d673
Alice <alicesb2006@gmail.com>
2024-09-08 12:31:12 -0300

    Adicionando sombra

--- src/navigation/AppNavigation.jsx
+++ src/navigation/AppNavigation.jsx
@@ -36,6 +36,11 @@ const AppNavigator = () => {
         tabBarInactiveTintColor: '#000',
         tabBarStyle: {
           backgroundColor: '#EDEDED',
+          shadowColor: '#000',
+          shadowOffset: { width: 0, height: 2 },
+          shadowOpacity: 0.5,
+          shadowRadius: 2,
+          elevation: 10,
         },
         tabBarLabelStyle: {
           display: 'none'
