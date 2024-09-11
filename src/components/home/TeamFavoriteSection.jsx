843c8ee3b20745dbb9096b7259cf35b254b78adc
Alice <alicesb2006@gmail.com>
2024-09-11 20:14:33 -0300

    Adicionando os estilos

--- src/components/home/TeamFavoriteSection.jsx
+++ src/components/home/TeamFavoriteSection.jsx
@@ -1,10 +1,12 @@
 // Importando dependencias
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { Text, FlatList, TouchableOpacity } from 'react-native';
 // Importando dados
 import sampleFormulaETeamsData from '../../constant/teamsData.jsx'; 
+// Importando Estilos
+import { HomePilots, HomePilotsText, HomeUpRaceContainer, HomeUpRaces, HomeUpText, HomeUpTextContainer, HomeUpViewMoreText } from '../../styles/HomeStyles.jsx';
 
-const TeamPointsSection = ({ teamName }) => {
+const TeamPointsSection = ({ teamName, navigation }) => {
   const [teamPoints, setTeamPoints] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -23,25 +25,35 @@ const TeamPointsSection = ({ teamName }) => {
   }, [teamName]);
 
   return (
-    <View>
-      <Text>Team Points</Text>
+    <HomeUpRaces>
+      <HomeUpTextContainer>
+        <Text>Pilots</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Races')}>
+          <HomeUpText>See All</HomeUpText>
+        </TouchableOpacity>
+      </HomeUpTextContainer>
       {loading ? (
         <Text>Loading team points...</Text>
       ) : teamPoints ? (
-        <View>
+        <HomeUpRaceContainer>
           <FlatList
             data={teamPoints.drivers}
             keyExtractor={item => item.name}
             renderItem={({ item }) => (
-              <Text>{item.name}: {item.points_last_season} points</Text>
+              < HomePilots>
+                <HomePilotsText>{item.name}: {item.points_last_season} points</HomePilotsText>
+                <HomeUpViewMoreText>View More</HomeUpViewMoreText >
+              </ HomePilots>
             )}
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            pagingEnabled
           />
-          <Text>Total Point's: {teamPoints.total_points}</Text>
-        </View>
+        </HomeUpRaceContainer>
       ) : (
         <Text>No team points data available.</Text>
       )}
-    </View>
+    </HomeUpRaces>
   );
 };
 
