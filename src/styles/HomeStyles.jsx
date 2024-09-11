2118b676af79656ac754b48d165edbf294bd62b2
Alice <alicesb2006@gmail.com>
2024-09-11 20:15:49 -0300

    Criando os estilos do Home

--- src/styles/HomeStyles.jsx
+++ src/styles/HomeStyles.jsx
@@ -8,6 +8,8 @@ import Colors from '../constant/Colors';
 export const HomeSafeView = styled(SafeAreaView)`
   flex: 1;
   background-color: ${Colors.ft_color};
+  width: 100%;
+  align-items: center;
 `;
 
 // Estilos da seleção de time
@@ -84,5 +86,90 @@ export const HomeText = styled.Text`
 export const HomeContainerText = styled.View`
     width: 90%;
     gap: 2px;
-    padding: 30px 0;
+    padding: 30px 10px;
+`
+
+// Estilos para próxima corrida
+
+export const HomeContainer = styled.View`
+    width: 100%;
+    gap: 10px;
+    background-color: ${Colors.ft_color};
+`
+
+export const HomeUpRaces = styled.View`
+    width: 100%;
+    padding: 10px;
+    gap: 5px;
+`
+export const HomeUpTextContainer = styled.View`
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 10px;
+    border-radius: 0 10px 0 0;
+    background-color: ${Colors.br_color};
+`;
+export const HomeUpText = styled.Text`
+    font-family: 'Montserrat';
+    text-transform: uppercase;
+    font-weight: 700;
+`
+
+export const HomeUpRaceContainer = styled(LinearGradient).attrs({
+    colors: [ Colors.red_color2, Colors.red_color], 
+    start: { x: 0, y: 2 },
+    end: { x: 1.2, y: 0 },
+    locations: [0.2,1],
+})`
+    width:100%;
+    padding: 20px;
+    flex-direction: row;
+    align-items: flex-end;
+    justify-content: space-between;
+    border-radius: 0 0 0 10px;
+
+`;
+
+export const HomeUpRaceTextContainer = styled.View`
+    gap: 10px;
+`
+
+export const HomeUpRaceImage = styled.Image`
+    height: 40px;
+    width: 60px;
+    border-radius: 0 10px 0 10px;
+`
+
+export const HomeUpRaceText = styled.Text`
+    font-family: 'Montserrat';
+    text-transform: uppercase;
+    color: ${Colors.ft_color};
+    font-size: 12px;
+    font-weight: 600;
+`
+
+export const HomeUpViewMoreText = styled.Text`
+    font-family: 'Montserrat';
+    text-transform: uppercase;
+    color: ${Colors.ft_color};
+    background-color: ${Colors.fill_color1};
+    font-size: 12px;
+    font-weight: 800;
+    border: solid 1px ${Colors.ft_color};
+    padding: 4px 7px;
+    border-radius: 50px;
+`
+// Estilos para os Pilotos
+
+export const HomePilots = styled.View`
+    width: 300px;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const HomePilotsText = styled.Text`
+    color: ${Colors.ft_color};
+    text-transform: uppercase;
+    font-weight: 800;
 `
