6c25d61671227abaf135db8ca6b009015990fa83
Alice <alicesb2006@gmail.com>
2024-09-16 20:02:39 -0300

    Alterando o estilo e criando as rotas para as outras telas

--- src/navigation/AppNavigation.jsx
+++ src/navigation/AppNavigation.jsx
@@ -1,12 +1,21 @@
+// Importando dependencias
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons'; // Importando ícones
+import { Dimensions } from 'react-native';
+// Importando ícones
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 // Importando Telas
 import HomeScreen from '../screens/HomeScreen';
 import RacesScreen from '../screens/RacesScreen'; 
 import ProfileScreen from '../screens/ProfileScreen'; 
+import TRacerScreen from '../screens/TRacerScreen';
+import RankingRace from '../screens/RankingRace';
 // Importando estilos
 import { NavigationTabIcon } from '../styles/NavigationStyles';
+
+
+// Obter largura da tela
+const { width } = Dimensions.get('window');
 
 const Tab = createBottomTabNavigator();
 
@@ -24,11 +33,15 @@ const AppNavigator = () => {
             iconName = 'flag-checkered'; 
           } else if (route.name === 'Profile') {
             iconName = 'account'; 
+          } else if (route.name === 'Ranking') {
+            iconName = 'trophy'; 
+          } else if (route.name === 'TRacer') {
+            iconName = 'racing-helmet'; 
           }
 
           return (
             <NavigationTabIcon size={size} color={color}>
-              <MaterialCommunityIcons name={iconName} size={size} color={color} />
+              <MaterialCommunityIcons name={iconName} size={iconName != 'racing-helmet' ? size : size + 12} color={color} />
             </NavigationTabIcon>
           );
         },
@@ -45,11 +58,14 @@ const AppNavigator = () => {
         tabBarLabelStyle: {
           display: 'none'
         },
-        headerShown: false
+        tabBarStyle: { height: 60 },
+        headerShown: false,
       })}
       initialRouteName="Home"
     >
       <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Ranking" component={RankingRace} />
+      <Tab.Screen name="TRacer" component={TRacerScreen} />
       <Tab.Screen name="Races" component={RacesScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
--- src/styles/NavigationStyles.jsx
+++ src/styles/NavigationStyles.jsx
@@ -5,7 +5,7 @@ export const NavigationTabIcon = styled.View`
     border-width: 2px;
     border-color: ${(props) => props.color != Colors.ft_color2 ? Colors.red_color : Colors.ft_color2};
     border-radius: ${(props) => props.size + 10}px;
-    padding: 5px; 
+    padding: 3px; 
     justify-content: center;
     align-items: center;
 `
