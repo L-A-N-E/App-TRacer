6a35ea5d55114a9020995b9be20b7711282a0288
Alice <alicesb2006@gmail.com>
2024-09-16 20:02:05 -0300

    Criando as telas de ranking e TRacer

---
+++ src/screens/RankingRace.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const RankingRace = () => {
+  return (
+    <View>
+        <Text>Ranking</Text>
+    </View>
+  )
+}
+
+export default RankingRace
---
+++ src/screens/TRacerScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const TRacerScreen = () => {
+  return (
+    <SafeAreaView>
+        <Text>TRACER</Text>
+    </SafeAreaView>
+  )
+}
+
+export default TRacerScreen
