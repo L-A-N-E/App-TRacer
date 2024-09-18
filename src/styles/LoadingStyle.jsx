32ac4ef1553fdc730dab615dc624e7ebe70018af
Alice <alicesb2006@gmail.com>
2024-09-18 15:17:06 -0300

    Criando estilo para o Loading

---
+++ src/styles/LoadingStyle.jsx
@@ -0,0 +1,11 @@
+// Importando dependencias
+import styled from 'styled-components/native';
+// Importando cores
+import Colors from '../constant/Colors';
+
+export const LoadingContainer = styled.View`
+    flex: 1;
+    background-color: ${Colors.ft_color};
+    justify-content: center;
+    align-items: center;
+`;
