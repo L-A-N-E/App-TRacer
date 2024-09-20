def4922f6bca1b218757a85531e8f814e1df5d62
Alice <alicesb2006@gmail.com>
2024-09-20 14:07:51 -0300

    Criando o estilo do header do navegador do TRacer

--- src/navigation/TRacerNavigation.jsx
+++ src/navigation/TRacerNavigation.jsx
@@ -1,17 +1,69 @@
+// Importando dependencias
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-// Importando telas
+import AntDesign from '@expo/vector-icons/AntDesign';
+// Importando as telas do aplicativo
 import TRacerScreen from '../screens/TRacerScreen'; 
 import PilotSelectionScreen from '../screens/PilotSelectionScreen'; 
 import PilotDetailScreen from '../screens/PilotsDetailsScreen';
+// Importando cores definidas em um arquivo de constantes
+import Colors from '../constant/Colors';
 
+// Criando um objeto Stack para navegação
 const Stack = createStackNavigator();
 
 const AppNavigator = () => (
-    <Stack.Navigator initialRouteName="TRacer">
-      <Stack.Screen name="TRacer" component={TRacerScreen} />
-      <Stack.Screen name="PilotSelection" component={PilotSelectionScreen} />
-      <Stack.Screen name="PilotDetail" component={PilotDetailScreen} />
+    <Stack.Navigator 
+      initialRouteName="TRacer" // Definindo a tela inicial do navegador
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: Colors.red_color, 
+          height: 100, 
+        },
+        headerTintColor: Colors.ft_color, 
+        headerTitleStyle: {
+          fontFamily: 'Montserrat-ExtraBold', 
+          fontSize: 20, 
+        },
+        headerTitleAlign: 'center', 
+      }}>
+      <Stack.Screen 
+        name="TRacer" // Nome da tela
+        component={TRacerScreen} // Componente que será renderizado
+        options={{ title: 'TRACER' }} // Título a ser exibido no cabeçalho
+      />
+      <Stack.Screen 
+        name="PilotSelection" // Nome da tela
+        component={PilotSelectionScreen} // Componente que será renderizado
+        options={({ navigation }) => ({
+          title: 'PILOTS', // Título da tela
+          headerLeft: () => ( // Personalizando o botão de voltar
+            <AntDesign 
+              name="left" 
+              size={24} 
+              color={Colors.ft_color} 
+              onPress={() => navigation.goBack()} 
+              style={{ marginLeft: 10 }} 
+            />
+          ),
+        })} 
+      />
+      <Stack.Screen 
+        name="PilotDetail" // Nome da tela
+        component={PilotDetailScreen} // Componente que será renderizado
+        options={({ navigation }) => ({
+          title: 'DETAILS', // Título da tela
+          headerLeft: () => ( // Personalizando o botão de voltar
+            <AntDesign 
+              name="left" 
+              size={24} 
+              color={Colors.ft_color} 
+              onPress={() => navigation.goBack()} 
+              style={{ marginLeft: 10 }} 
+            />
+          ),
+        })} 
+      />
     </Stack.Navigator>
 );
 
