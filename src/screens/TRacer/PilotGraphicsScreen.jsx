82a49f082606b677ef514f0fb40f04a496fc1d50
Alice <alicesb2006@gmail.com>
2024-10-13 12:29:39 -0300

    Criando a página dos gráficos

---
+++ src/screens/TRacer/PilotGraphicsScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const PilotGraphicsScreen = () => {
+  return (
+    <View>
+      <Text>PilotGraphicsScreen</Text>
+    </View>
+  )
+}
+
+export default PilotGraphicsScreen
