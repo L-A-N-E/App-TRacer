4bc7167157e7b9ceed69a3462f9c705d613e5320
Alice <alicesb2006@gmail.com>
2024-10-13 12:28:42 -0300

    Criando o estilo dos detalhes

--- src/styles/PilotsStyles.jsx
+++ src/styles/PilotsStyles.jsx
@@ -1,6 +1,6 @@
 // Importando dependencias
 import styled from 'styled-components';
-import { Dimensions } from 'react-native';
+import { Dimensions, TouchableOpacity } from 'react-native';
 // Importando cores
 import Colors from '../constant/Colors';
 
@@ -107,4 +107,100 @@ export const PilotBold = styled.Text`
     font-family: 'Montserrat-ExtraBold';
     color: ${Colors.ft_color};
     font-size: 20px;
+`
+
+// Estilo para o detalhes dos pilotos
+
+export const SectionDetails = styled.View`
+  flex: 1;
+`;
+
+export const ImageContainer = styled.View`
+  justify-content: flex-end;
+  align-items: center;
+  height: 62%; 
+  position: relative;
+  background-color: ${Colors.br_color};
+`;
+
+export const Faixa = styled.View`
+  position: absolute;
+  top: 0;
+  width: 100px; 
+  height: 100%; 
+  background-color: ${Colors.red_color}; 
+  z-index: 0; 
+`;
+
+export const ShowPilotImage = styled.Image`
+  width: 300px;
+  height: 300px;
+  z-index: 1; 
+`;
+
+export const ContainerPilotName = styled.View`
+    background-color: ${Colors.br_color};
+    display: flex;
+    height: 10%;
+    justify-content: center; 
+    align-items: center; 
+    elevation: 5;
+`;
+
+export const DetailsPilotName = styled.Text`
+    font-family: 'Montserrat-ExtraBold';
+    text-transform: uppercase;
+    color: ${Colors.red_color};
+    font-size: 26px;
+    text-align: center; 
+`;
+
+export const InfoContainer = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+`
+
+export const DetailsContainer = styled.View`
+    display: flex;
+    flex-direction: row;
+    gap: 40px;
+`
+
+export const ButtonDetails1 = styled(TouchableOpacity)`
+    padding: 10px 20px;
+    border-radius: 0 0 0 10px;
+    background-color: ${Colors.red_color};
+`
+
+export const ButtonDetails2 = styled(TouchableOpacity)`
+    padding: 10px 20px;
+    border-radius: 0 10px 0 0;
+    background-color: ${Colors.red_color};
+`
+
+export const ButtonText = styled.Text`
+    font-family: 'Montserrat-ExtraBold';
+    text-transform: uppercase;
+    color: ${Colors.ft_color};
+    font-size: 16px;
+    text-align: center; 
+`;
+
+export const TextUpper = styled.Text`
+    font-family: 'Montserrat-ExtraBold';
+    text-transform: uppercase;
+    color: ${Colors.red_color};
+    font-size: 40px;
+    text-align: center; 
+`
+
+export const TextInfo = styled.Text`
+    width: 140px;
+    font-family: 'Montserrat-ExtraBold';
+    text-transform: uppercase;
+    color: ${Colors.red_color};
+    font-size: 24px;
+    text-align: center;
 `
