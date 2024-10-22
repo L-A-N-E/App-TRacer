888376f56b2b61638ac08a55a85baca93a2607e8
Alice <alicesb2006@gmail.com>
2024-10-22 19:57:42 -0300

    Fazendo a tela de profile

--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,13 +1,81 @@
-// Página sujeita a alteração
-// Importando dependencias
-import React from 'react';
-// Importando dependencias
+// Importando dependências
+import React, { useEffect, useState } from 'react';
+import { View, Text, Image, Button, StyleSheet, TouchableOpacity } from 'react-native'; 
+import { ProfileAvatar, ProfileBanner, ProfileButton, ProfileButtonText, ProfileContainerInfo, ProfileInfo, ProfileSettings, ProfileText, ProfileTextContainer, ProfileTitle, ProfileUsername, ProfileView } from '../styles/ProfileStyles';
+import { onAuthStateChanged, signOut } from '@react-native-firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+import { auth, db } from '../firebase/firebaseConfig';
 import ProfileLogOut from '../components/ProfileLogOut';
 
 const ProfileScreen = () => {
+
+  const [userData, setUserData] = useState({});
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth); // Chama a função de logout do Firebase
+      console.log('Usuário deslogado com sucesso!');
+      // Aqui você pode redirecionar o usuário para outra tela, se necessário
+    } catch (error) {
+      console.error('Erro ao deslogar:', error);
+    }
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (currentUser) => {
+        if (currentUser) {
+            const uid = currentUser.uid;
+            const docRef = doc(db, "users", uid);
+            const docSnap = await getDoc(docRef);
+            setUserData(docSnap.data());
+        }
+    });
+
+    return () => unsubscribe();
+}, [auth]);
+
   return (
-      <ProfileLogOut/>
+    <ProfileView>
+      {/* Banner com avatar */}
+      <ProfileBanner>
+        <ProfileAvatar
+          source={{ uri: !userData.avatarImg ? 'https://media.istockphoto.com/id/1337144146/pt/vetorial/default-avatar-profile-icon-vector.jpg?s=612x612&w=0&k=20&c=_XeYoSJQIN7GrE08cUQDJCo3U7yvoEp5OKpbhQzpmC0=' : userData.avatarImg }} // Substitua pela URL do seu avatar
+        />
+      </ProfileBanner>
+      {/* Informações do perfil */}
+      <ProfileInfo>
+        <ProfileUsername>{userData.username}</ProfileUsername>
+        <ProfileContainerInfo>
+          <ProfileTextContainer>
+            <ProfileTitle>Email: </ProfileTitle>
+            <ProfileText>{userData.email}</ProfileText>
+          </ProfileTextContainer>
+          <ProfileTextContainer>
+            <ProfileTitle>Favorite Team: </ProfileTitle>
+            <ProfileText>{userData.favoriteTeam}</ProfileText>
+          </ProfileTextContainer>
+            <View>
+              <ProfileTitle>Settings</ProfileTitle>
+              <ProfileSettings>
+                <TouchableOpacity>
+                  <ProfileText>Change Avatar</ProfileText>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <ProfileText>Change Username</ProfileText>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                  <ProfileText>Change Favorite Team</ProfileText>
+                </TouchableOpacity>
+              </ProfileSettings>
+            </View>
+            <ProfileButton onPress={handleLogout}>
+              <ProfileButtonText>LogOut</ProfileButtonText>
+            </ProfileButton>
+        </ProfileContainerInfo>
+      </ProfileInfo>
+    </ProfileView>
   );
 };
+
 
 export default ProfileScreen;
