e78d15ad4eb61cb9c9c04765358f2d70c7056ed4
Alice <alicesb2006@gmail.com>
2024-10-22 19:57:10 -0300

    Ajustando a configuração do firebase

--- src/firebase/firebaseConfig.jsx
+++ src/firebase/firebaseConfig.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
@@ -22,5 +23,6 @@ const auth = initializeAuth(app, {
     persistence: getReactNativePersistence(ReactNativeAsyncStorage)
   });
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { app, auth, db };
+export { app, auth, db, storage };
