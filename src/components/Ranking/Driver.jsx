739ef1184e9753e8ab86daf05e196f8b06c5b23b
Alice <alicesb2006@gmail.com>
2024-10-22 20:59:22 -0300

    Criando os componentes

--- src/components/Ranking/Driver.jsx
+++ src/components/Ranking/Driver.jsx
@@ -4,10 +4,11 @@ import React from 'react'
 const Driver = ({ item }) => {
   return (
     <View>
-      <Text>{item.name}</Text>
-      <Text>Pontos: {item.points_last_season}</Text>
-      <Text>Ranking: {item.final_ranking}</Text>
-      <Text>Time: {item.team}</Text>
+      <Text>{item.final_ranking}</Text>
+      <View>
+        <Text>{item.name}</Text>
+        <Text>Points: {item.points_last_season}</Text>
+      </View>
     </View>
   )
 }
--- src/components/Ranking/Team.jsx
+++ src/components/Ranking/Team.jsx
@@ -1,12 +1,15 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
 
 const Team = ({ item, index }) => {
   return (
     <View>
     <Text>{index + 1}</Text>
-    <Text>{item.team}</Text>
-    <Text>{item.total_points}</Text>
+    <View>
+      <Text>{item.team}</Text>
+      <Text>Points: {item.total_points}</Text>
+    </View>
+    <Image source={item.imageSource} />
   </View>
   )
 }
