58beec64f41fcaa2b7f63d463fe8933fa690628d
Alice <alicesb2006@gmail.com>
2024-10-23 20:09:57 -0300

    Alterando as cores do ErrorStyle

--- src/styles/ErrorStyle.jsx
+++ src/styles/ErrorStyle.jsx
@@ -1,18 +1,24 @@
 // Importando dependencias
 import styled from 'styled-components';
-// Impotando cores
-import Colors from '../constant/Colors';
+// Importa o hook para acessar as cores
+import { useColors } from '../context/ColorsContext'; 
+
+// Custom hook para usar as cores no styled-components
+const useThemeColors = () => {
+  const colors = useColors();
+  return colors;
+};
 
 export const ErrorContainer = styled.View`
     flex: 1;
-  background-color: ${Colors.ft_color};
+  background-color: ${() => useThemeColors().ft_color};
   width: 100%;
   align-items: center;
   justify-content: center;
 `
 
 export const ErrorText = styled.Text`
-color: ${Colors.ft_color2};
+color: ${() => useThemeColors().ft_color2};
   text-transform: uppercase;
   font-family: 'Montserrat-Bold';
   font-size: 24px;
