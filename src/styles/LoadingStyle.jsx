188d612494803332b9e6c7ebfda1ce4ae028aa5c
Alice <alicesb2006@gmail.com>
2024-10-23 20:10:43 -0300

    Alterando as cores do LoadingStyle

--- src/styles/LoadingStyle.jsx
+++ src/styles/LoadingStyle.jsx
@@ -1,11 +1,17 @@
 // Importando dependencias
 import styled from 'styled-components/native';
-// Importando cores
-import Colors from '../constant/Colors';
+// Importa o hook para acessar as cores
+import { useColors } from '../context/ColorsContext'; 
+
+// Custom hook para usar as cores no styled-components
+const useThemeColors = () => {
+  const colors = useColors();
+  return colors;
+};
 
 export const LoadingContainer = styled.View`
     flex: 1;
-    background-color: ${Colors.ft_color};
+    background-color: ${() => useThemeColors().ft_color};
     justify-content: center;
     align-items: center;
 `;
